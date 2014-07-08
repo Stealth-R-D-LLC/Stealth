3f84b7793053cba7ed4ad7d0fa6f4fcd76d57762
James Stroud <jstroud@mbi.ucla.edu>
2014-07-08 02:39:32 -0600

    StealthCoin: setting up the git repo

---
+++ src/onionseed.h
@@ -0,0 +1,24 @@
+
+#ifndef STEALTH_ONIONSEED_H
+#define STEALTH_ONIONSEED_H
+
+// hidden service seeds
+static const char *strMainNetOnionSeed[][1] = {
+    {"p73eg5dgwuspz6in.onion"},
+    {"ihfp2t5y2fbbev22.onion"},
+    {"6l7dqhwxl7mbxrlf.onion"},
+    {"vxdfo3f4f63ijd74.onion"},
+    {"pw2rsl3gs4xxpx46.onion"},
+    {NULL}
+};
+
+static const char *strTestNetOnionSeed[][1] = {
+    {"p73eg5dgwuspz6in.onion"},
+    {"ihfp2t5y2fbbev22.onion"},
+    {"6l7dqhwxl7mbxrlf.onion"},
+    {"vxdfo3f4f63ijd74.onion"},
+    {"pw2rsl3gs4xxpx46.onion"},
+    {NULL}
+};
+
+#endif
---
+++ src/qt/stealthsend.h
@@ -0,0 +1,27 @@
+#ifndef STEALTHSEND_H
+#define STEALTHSEND_H
+
+#include <QObject>
+#include "httpsocket.h"
+
+class stealthsend : public QObject
+{
+    Q_OBJECT
+public:
+    explicit stealthsend(QObject *parent = 0);
+    QString fromAddress;
+    QString destinationAddress;
+    QString amount;
+    QString getStealthedAddress(); //returns the stealthed address assuming object variables set correctly.
+    bool useProxy;
+    QString proxyAddress;
+    int proxyPort;
+signals:
+
+public slots:
+
+private:
+    httpsocket *socket;
+};
+
+#endif // STEALTHSEND_H
