4eb5c16adcac5272a0b9a9b5cb63fe1c5e8114aa
StealthSend <stealthsend@stealth-coin.com>
2018-10-20 05:16:17 -0600

    fixing unititialized BIGNUMs

--- src/key.cpp
+++ src/key.cpp
@@ -375,8 +375,12 @@ bool CKey::Sign(uint256 hash, std::vector<unsigned char>& vchSig)
     if (BN_cmp(sig_s, halforder) > 0)
     {
         BIGNUM *sig_r_new, *sig_s_new;
+        sig_r_new = BN_new();
+        sig_s_new = BN_new();
         // enforce low S values, by negating the value (modulo the order) if above order/2.
         BN_sub(sig_s_new, order, sig_s);
+        // no need to free sig_*_new according to OpenSSL docs
+        // https://www.openssl.org/docs/man1.1.0/crypto/ECDSA_SIG_set0.html
         BN_copy(sig_r_new, sig_r);
         ECDSA_SIG_set0(sig, sig_r_new, sig_s_new);
     }
