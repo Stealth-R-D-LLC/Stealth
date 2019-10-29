88c662046e911ce97d4db805d3718af54799c911
StealthSend <stealthsend@stealth-coin.com>
2019-10-29 16:06:21 -0700

    catching up to master

--- contrib/pyHash9/pyHash9.cpp
+++ contrib/pyHash9/pyHash9.cpp
@@ -1,9 +1,12 @@
-// Copyright (c) 2016-2018, Stealth R&D LLC
+// Copyright (c) 2016-2019 Stealth R&D LLC
 
 #include <stddef.h>
+
 #include "hashblock.h"
 
 #include <Python.h>
+
+static const int HASHLEN = 32;
 
 PyObject* hash9(PyObject *self, PyObject *args)
 {
@@ -19,15 +22,42 @@ PyObject* hash9(PyObject *self, PyObject *args)
 
     uint256 hash = Hash9(stringIn, stringIn + size);
 
-    // printf("Hash is %s\n", hash.ToString().c_str());
-
     return PyString_FromString(hash.ToString().c_str());
+}
+
+PyObject* hash(PyObject *self, PyObject *args)
+{
+    char* stringIn;
+    int size = 0;
+
+    /* can't parse args */
+    if (! PyArg_ParseTuple(args, "s#", &stringIn, &size)) {
+      PyErr_SetString(PyExc_TypeError,
+        "method expects a string(s)") ;
+      return NULL ;
+    }
+
+    uint256 h = Hash9(stringIn, stringIn + size);
+
+    char bytes[HASHLEN + 1];
+    bytes[HASHLEN] = 0x00;
+
+    // uint256 internal representation is reversed
+    int i = HASHLEN - 1;
+    for (unsigned char *c = h.begin(); c != h.end(); ++c)
+    {
+        bytes[i] = (char)*c;
+        i -= 1;
+    }
+
+    return PyString_FromStringAndSize(bytes, HASHLEN);
 }
 
 
 static struct PyMethodDef pyHash9_methods[] =
 {
-    {"hash9", hash9, 1, "get the X13 hash"},
+    {"hash9", hash9, 1, "get the X13 hash as hex"},
+    {"hash", hash, 1, "get the X13 hash"},
     {NULL, NULL}
 };
 
