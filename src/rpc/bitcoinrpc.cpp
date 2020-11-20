a5b08e3e0c78ad5e0eeba50ff3094e163e0008cf
StealthSend <stealthsend@stealth-coin.com>
2020-11-20 04:39:50 -0800

    fixing rpc value conversions

--- src/rpc/bitcoinrpc.cpp
+++ src/rpc/bitcoinrpc.cpp
@@ -1377,13 +1377,13 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "getaddressinoutspg"       && n > 2)
                                 ConvertTo<boost::int64_t>(params[2]);
     if (strMethod == "getaddressinoutspg"       && n > 3)
-                                ConvertTo<boost::int64_t>(params[3]);
+                                ConvertTo<bool>(params[3]);
     if (strMethod == "getrichlistsize"          && n > 0)
                                 ConvertTo<boost::int64_t>(params[0]);
     if (strMethod == "getrichlist"              && n > 0)
                                 ConvertTo<boost::int64_t>(params[0]);
     if (strMethod == "getrichlist"              && n > 1)
-                                ConvertTo<bool>(params[1]);
+                                ConvertTo<boost::int64_t>(params[1]);
 
     return params;
 }
