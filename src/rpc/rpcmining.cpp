619ebd786c7e8bbbb2f84e64b5d9362fb38051e0
StealthSend <stealthsend@stealth-coin.com>
2022-07-21 04:04:20 -0700

    fixed typo for max block size

--- src/rpc/rpcmining.cpp
+++ src/rpc/rpcmining.cpp
@@ -602,7 +602,7 @@ Value getblocktemplate(const Array& params, bool fHelp)
     result.push_back(Pair("mutable", aMutable));
     result.push_back(Pair("noncerange", "00000000ffffffff"));
     result.push_back(Pair("sigoplimit", (int64_t)chainParams.MAX_BLOCK_SIGOPS));
-    result.push_back(Pair("sizelimit", (int64_t)chainParams.FEELESS_MAX_BLOCK_SIZE));
+    result.push_back(Pair("sizelimit", (int64_t)chainParams.MAX_BLOCK_SIZE));
     result.push_back(Pair("curtime", (int64_t)pblock->nTime));
     result.push_back(Pair("bits", HexBits(pblock->nBits)));
     result.push_back(Pair("height", (int64_t)(pindexPrev->nHeight+1)));
