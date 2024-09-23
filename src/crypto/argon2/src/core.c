6822fbde01f7c0631a8a3993b45565cf6b78b753
StealthSend <StealthSend stealthsend@stealth-coin.com>
2024-09-23 01:42:22 -0700

    update argon2 and hashblock for newer compilers

--- src/crypto/argon2/src/core.c
+++ src/crypto/argon2/src/core.c
@@ -419,9 +419,11 @@ int validate_inputs(const argon2_context *context) {
         }
     }
 
+    /* Always false, unneeded.
     if (ARGON2_MIN_PWD_LENGTH > context->pwdlen) {
       return ARGON2_PWD_TOO_SHORT;
     }
+     */
 
     if (ARGON2_MAX_PWD_LENGTH < context->pwdlen) {
         return ARGON2_PWD_TOO_LONG;
@@ -448,9 +450,11 @@ int validate_inputs(const argon2_context *context) {
             return ARGON2_SECRET_PTR_MISMATCH;
         }
     } else {
+        /* Always false, unneeded.
         if (ARGON2_MIN_SECRET > context->secretlen) {
             return ARGON2_SECRET_TOO_SHORT;
         }
+         */
         if (ARGON2_MAX_SECRET < context->secretlen) {
             return ARGON2_SECRET_TOO_LONG;
         }
@@ -462,9 +466,11 @@ int validate_inputs(const argon2_context *context) {
             return ARGON2_AD_PTR_MISMATCH;
         }
     } else {
+        /* Always false, unneeded.
         if (ARGON2_MIN_AD_LENGTH > context->adlen) {
             return ARGON2_AD_TOO_SHORT;
         }
+        */
         if (ARGON2_MAX_AD_LENGTH < context->adlen) {
             return ARGON2_AD_TOO_LONG;
         }
@@ -475,9 +481,11 @@ int validate_inputs(const argon2_context *context) {
         return ARGON2_MEMORY_TOO_LITTLE;
     }
 
+    /* Always false, unneeded.
     if (ARGON2_MAX_MEMORY < context->m_cost) {
         return ARGON2_MEMORY_TOO_MUCH;
     }
+    */
 
     if (context->m_cost < 8 * context->lanes) {
         return ARGON2_MEMORY_TOO_LITTLE;
