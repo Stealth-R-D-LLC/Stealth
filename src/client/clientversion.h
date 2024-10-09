fe5f516ee50d405ae1c798f5ddb54172b78b9e47
StealthSend <stealthsend@stealth-coin.com>
2024-10-08 21:26:43 -0700

    tag to mark last of v3.1

--- src/client/clientversion.h
+++ src/client/clientversion.h
@@ -9,7 +9,7 @@
 #define CLIENT_VERSION_MAJOR       3
 #define CLIENT_VERSION_MINOR       1
 #define CLIENT_VERSION_REVISION    8
-#define CLIENT_VERSION_BUILD       0
+#define CLIENT_VERSION_BUILD       1
 
 // cloners: add your new forks higher than highest here
 //          keep existing
